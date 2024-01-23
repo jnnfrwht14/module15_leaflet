62a2a72b60b9efcc4516a537dd9e4c0176e343ef
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2024-01-22 22:40:29 -0600

    0122a delta

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,3 +6,49 @@ d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
 });
+
+function createFeatures(earthquakeData) {
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+  
+    function colorChart(depth) {
+        return depth > 90 ? '#840006':
+            depth > 70 ? '#DD4132':
+                depth > 50 ? '#E4BF45':
+                    depth > 30 ? '#F3E779':
+                        depth > 10 ? '#e2e000':
+                            '#e2e000';
+    }
+
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+            radius: feature.properties.mag * 5,
+            fillColor: colorChart(feature.geometry.coordinates[2]),
+            color: '#f7b962',
+            weight: 1.42,
+            opacity: 1,
+            fillOpacity: 0.62
+        });
+      }
+    });
+
+    createMap(earthquakes);
+}
+
+function createMap(earthquake) {
+    // Create the base layers.
+    
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+}
