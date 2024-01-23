7af07e7d39016cd827cfef57ad817044dd46145f
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2024-01-22 22:12:30 -0600

    0122 deltas

---
+++ static/js/logic.js
@@ -0,0 +1,8 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
