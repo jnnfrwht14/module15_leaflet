43d8d4173d1262bc7b33fc0d5070468ac1bd7735
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2024-01-25 21:44:51 -0600

    0125 deltas

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
@@ -21,7 +21,7 @@ function createFeatures(earthquakeData) {
                 depth > 50 ? '#E4BF45':
                     depth > 30 ? '#F3E779':
                         depth > 10 ? '#e2e000':
-                            '#e2e000';
+                            '#80c8b9';
     }
 
   let earthquakes = L.geoJSON(earthquakeData, {
@@ -41,14 +41,62 @@ function createFeatures(earthquakeData) {
     createMap(earthquakes);
 }
 
-function createMap(earthquake) {
+function createMap(earthquakes) {
     // Create the base layers.
     
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+    })
 
     let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     });
-}
+
+    let baseMaps = {
+        'Street Map': street,
+        'Topographic Map': topo
+    };
+
+    let overlayMaps = {
+        Earthquakes: earthquakes
+    };
+    let myMap = L.map("map", {
+        center: [
+          41.623655, -93.592339
+        ],
+        zoom: 4.2,
+        layers: [street, earthquakes]
+      });
+
+      L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+
+    // Set up the legend.
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    let limits = geojson.options.limits;
+    let colors = geojson.options.colors;
+    let labels = [];
+
+    // Add the minimum and maximum.
+    let legendInfo = "<h1>Earthquakes<br />(ages 6-17)</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
+};
